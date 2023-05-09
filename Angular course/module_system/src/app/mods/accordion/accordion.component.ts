7e828ec074f18854eb350f1346260f7f7a636d4b
Godfrey96 <ngwatlegodfrey@gmail.com>
2023-05-09 08:41:53 +0200

    section 10 completed

--- Angular course/module_system/src/app/mods/accordion/accordion.component.ts
+++ Angular course/module_system/src/app/mods/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./accordion.component.css']
 })
 export class AccordionComponent {
+
+  @Input() items: any = [];
+  openedItemIndex = 0;
+
+  onClick(index: number) {
+    if (index === this.openedItemIndex) {
+      this.openedItemIndex = -1;
+    } else {
+      this.openedItemIndex = index;
+    }
+  }
 
 }
